e78c4645f4b9f3f730318073c139faa47a52878e
Ivanitsky-Oleksii <goleecindj@gmail.com>
2023-02-08 21:18:59 +0200

    add image slider

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -7,16 +7,11 @@ import { Container } from '@mui/system'
 function Carousels()
 {
     return (
-        <Container maxWidth="xl" className="carousel-container"sx={{
-            padding: '0px 0',
-        }}
-    >
         <Carousel>
             {
                 sliderArray.map( item => <Item key={item.id} item={item} /> )
             }
         </Carousel>
-        </Container>
     )
 }
 export default Carousels 
--- src/components/Carousel/Item.js
+++ src/components/Carousel/Item.js
@@ -6,7 +6,7 @@ function Item({item})
 {
     return (
         <Paper>
-            <img src={item.image} alt="fake page" style={{width:"100%", height:"576px", marginTop:"0px"  }}/>
+            <img src={item.image} alt="fake page" style={{width:"100%", height:"28vh", marginTop:"1px"  }}/>
         </Paper>
     )
 }
