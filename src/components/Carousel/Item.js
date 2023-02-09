5fcc07fe1996a8f4fbaeda45bcf3d45a4f68a118
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-09 10:18:49 +0200

    add img for carousel

--- src/components/Carousel/Item.js
+++ src/components/Carousel/Item.js
@@ -6,7 +6,7 @@ function Item({item})
 {
     return (
         <Paper>
-            <img src={item.image} alt="fake page" style={{width:"100%", height:"28vh", marginTop:"1px"  }}/>
+            <img src={item.image} alt="fake page" style={{width:"100%", height:"28vh", marginTop:"0px"  }}/>
         </Paper>
     )
 }
