854b0e9635bb54388502ae459142335a997d6faf
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-10 14:33:29 +0200

    fix carousel

--- src/components/Carousel/Item.js
+++ src/components/Carousel/Item.js
@@ -6,7 +6,7 @@ function Item({item})
 {
     return (
         <Paper>
-            <img src={item.image} alt="fake page" style={{width:"100%", height:"28vh", margin:"0px"  }}/>
+            <img src={item.image} alt="fake page" style={{width:"100%", height:"28vh", margin:"-10px"  }}/>
         </Paper>
     )
 }
