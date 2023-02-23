68e44b0eba8b4a53a01a29c109ed406b929cf137
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-02-23 14:35:37 +0200

    add playlist and player for radio

--- src/utils/rp4Array.ts
+++ src/utils/rp4Array.ts
@@ -13,12 +13,12 @@ const rp4Array: Product[] = [
         id:4,
         title: 'GAIA: Tuvan (AVIRA Remix)',
 
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus debitis magnam at alias ea, dolorem laudantium! Maxime excepturi ea eos. Impedit delectus molestias totam quisquam odit repellat debitis velit laborum.',
+        description: 'Having remixed a smorgasbord of classics in the past few years, AVIRA puts his hand to one of the most beloved trance tracks of all time. Premiered by the Canadian DJ and producer at Ushuaia, Ibiza and every club or festival set since, this phenomenal, melodic-techno-geared rendition of GAIAs Tuvan ensures goosebumps all over.',
 
         data: 'January 16, 2023',
 
         image:"/images/21.jpg",
-        url:"https://wao138377.lnk.to/TheGodsDiscoAW",
+        url:"https://armd1702.lnk.to/TuvanARAW",
         link:"rpage4"
     },
     
--- src/utils/rp6Array.ts
+++ src/utils/rp6Array.ts
@@ -13,12 +13,12 @@ const rp6Array: Product[] = [
         id:6,
         title: 'Lange pres. Firewall: Sincere',
 
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus debitis magnam at alias ea, dolorem laudantium! Maxime excepturi ea eos. Impedit delectus molestias totam quisquam odit repellat debitis velit laborum.',
+        description: 'Following the recent rendition of Tomac, Irish producer William Byrne turns in an ace reimagination of Langes 2003 classic Sincere (as Firewall). Erring on the side of tech trance whilst simultaneously cultivating its uplifting inclination, this remix already grabbed the spotlight on Armin van Buurens A State Of Trance radio show and is set to remain a fan favorite for a long time to come',
 
         data: 'January 16, 2023',
         
         image:"/images/20.jpg",
-        url:"https://wao138377.lnk.to/TheGodsDiscoAW",
+        url:"https://wao138374.lnk.to/SincereWBRWA",
         link:"rpage6"
     },
     
